adf3332ccdaeb3209d6ed4ba0975dd339ef0cdcc
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-21 19:34:43 -0500

    Update modelFFNN.py

--- apps/modelFFNN.py
+++ apps/modelFFNN.py
@@ -17,7 +17,7 @@ def app():
     #start = '2004-08-18'
     #end = '2022-01-20'
     start = st.date_input('Start' , value= dt.datetime(2004,8,18))
-    end = st.date_input('End' , value=pd.to_datetime('today'))
+    end = st.date_input('End' , value=dt.datetime(2020,8,18))
 
     st.title('Predicción de tendencia de acciones')
 
